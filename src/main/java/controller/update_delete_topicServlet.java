0224285910ba6b0a352969d976ad9fc3723b515c
JoyceLIN0811 <chihaya0811@gmail.com>
2020-04-26 23:43:15 +0800

    third update

--- src/main/java/controller/update_delete_topicServlet.java
+++ src/main/java/controller/update_delete_topicServlet.java
@@ -27,16 +27,16 @@ import service.impl.TopicServiceImpl;
 @WebServlet("/_04_topic/update_delete_topic.do")
 public class update_delete_topicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doPost(req, resp);	
+		doPost(req, resp);
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		req.setCharacterEncoding("UTF-8");
 
 		HttpSession session = req.getSession(false);
-		
+
 		if (session == null) {
 //			resp.sendRedirect(resp.encodeRedirectURL(req.getContextPath() +"/_02_login/login.jsp"));
 			req.getRequestDispatcher("/_02_login/login.jsp").forward(req, resp);
@@ -47,14 +47,14 @@ public class update_delete_topicServlet extends HttpServlet {
 			resp.sendRedirect(getServletContext().getContextPath() + "/index.jsp");
 			return;
 		}
-				
+
 		String username = mb.getUsername();
 		String title = req.getParameter("title");
 		String content = req.getParameter("content");
 		String tidstr = req.getParameter("topicid");
 		String cidstr = req.getParameter("categoryid");
 		String action = req.getParameter("action");
-		
+
 		System.out.println(req.getParameter("topicid"));
 		System.out.println(req.getParameter("title"));
 		System.out.println(req.getParameter("content"));
@@ -62,66 +62,39 @@ public class update_delete_topicServlet extends HttpServlet {
 		System.out.println(req.getParameter("action"));
 		System.out.println(mb.getUsername());
 
+		int topicid = 0;
+		int categoryid = 0;
 
-		int topicid = 0 ;
-		int categoryid = 0 ;
-
-		try{
+		try {
 			// 進行資料型態的轉換
 			topicid = Integer.parseInt(tidstr.trim());
 			categoryid = Integer.parseInt(cidstr.trim());
-		} catch(NumberFormatException e){
-			throw new ServletException(e); 
+		} catch (NumberFormatException e) {
+			throw new ServletException(e);
 		}
-	
-		
-		TopicDaoImpl_Jdbc tpl = new TopicDaoImpl_Jdbc();
-		if(action=="delete") {
-		int n = tpl.deleteTopic(topicid);
-		if (n == 1) {
-			session.setAttribute("BookDeleteMsg", "文章(" + title + ")刪除成功");
-		} else {
-			session.setAttribute("BookDeleteMsg", "文章(" + title + ")刪除失敗");
-		}
-		resp.sendRedirect("mytopiclist");
-		return;}
-		
-		TopicBean tbn = new TopicBean();
 
+		TopicDaoImpl_Jdbc tpl = new TopicDaoImpl_Jdbc();
+		if (action == "delete") {
+			int n = tpl.deleteTopic(topicid);
+			if (n == 1) {
+				session.setAttribute("BookDeleteMsg", "文章(" + title + ")刪除成功");
+			} else {
+				session.setAttribute("BookDeleteMsg", "文章(" + title + ")刪除失敗");
+			}
+			return;
+
+		} else if (action == "update") {
+
+			TopicBean tbn = new TopicBean();
 
 			tbn.setTopicid(topicid);
 			tbn.setCategoryid(categoryid);
 			tbn.setTitle(title);
 			tbn.setContent(content);
-			tpl.updateTopic(tbn); 
-	        RequestDispatcher rd = req.getRequestDispatcher("mytopiclist");
-		    rd.forward(req, resp);
-		    return;
-//		}
-
-		
-//		tbn.setTopicid(topicid);
-//		tbn.setUsername(username);
-//		tbn.setCategoryid(categoryid);
-//		tbn.setTitle(title);
-//		tbn.setContent(content);
-//		tpl.saveTopic(tbn);
-//		
-//		
-//		List<TopicBean> page = tpl.getAlltopic();
-//		System.out.println( page);
-//		req.setAttribute("title_list", page);
-//		
-//		long num = tpl.getRecordCounts();
-//		System.out.println( num);
-//		req.setAttribute("TopicsTotalNum", num);
-//		req.getRequestDispatcher("/_03_addtopics/update_delete_topic.jsp").forward(req, resp);
-
-		
-//		RequestDispatcher rd = req.getRequestDispatcher("/_04_topic/topiclist.jsp");
-//		rd.forward(req, resp);
-//		req.getRequestDispatcher("/fragment/top.jsp").forward(req, resp);
-
+			tpl.updateTopic(tbn);
+			return;
+		}
+		resp.sendRedirect("mytopiclist");
 
 	}
 }
